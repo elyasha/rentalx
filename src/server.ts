933ec24c0bc0aff52a47a40faf51ecf66fb2449e
Matheus Elyasha <matheuselyasha@gmail.com>
2021-03-20 13:23:07 -0300

    Adicionando perfil para debugar aplicação

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,17 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (request, response) => {
     return response.json({message: "Hello world!"});
-})
+});
+
+app.post("/courses", (request, response) => {
+    // console.log(request.body);
+    const { name } = request.body;
+    // console.log(name);
+    return response.json({name});
+});
 
 app.listen(3333, () => console.log("Server is running!"));
