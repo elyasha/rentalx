cb3effb8d8955c1f3f8395231686cb0dd29b3213
Matheus Elyasha <matheuselyasha@gmail.com>
2021-03-20 14:09:58 -0300

    Listando as categorias

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,26 +1,24 @@
 import { Router } from 'express';
 import { v4 as uuidV4 } from 'uuid';
 import { Category } from '../model/Category';
+import { CategoriesRepository } from '../repositories/CategoriesRepository';
 
 const categoriesRoutes = Router();
-
-const categories: Category[] = [];
+const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (request, response) => {
     const { name, description } = request.body;
 
-    const category = new Category();
+    categoriesRepository.create({name, description});
 
-    Object.assign(category, {
-        name,
-        description,
-        created_at: new Date()
-    })
-
-    categories.push(category);
-
-    return response.status(201).json({ category });
+    return response.status(201).send();
 
 });
+
+
+categoriesRoutes.get("/", (request, response) => {
+    const all = categoriesRepository.list();
+    return response.json(all);
+})
 
 export { categoriesRoutes };
