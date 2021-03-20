4cb86fce94ffd4a14e8971cc61ce12974cccbe91
Matheus Elyasha <matheuselyasha@gmail.com>
2021-03-20 13:34:00 -0300

    Criando categoria de carro

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,10 @@
 import express from 'express';
+import { categoriesRoutes } from './routes/categories.routes';
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (request, response) => {
-    return response.json({message: "Hello world!"});
-});
-
-app.post("/courses", (request, response) => {
-    // console.log(request.body);
-    const { name } = request.body;
-    // console.log(name);
-    return response.json({name});
-});
+app.use(categoriesRoutes);
 
 app.listen(3333, () => console.log("Server is running!"));
