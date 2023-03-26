5777769adb405a003d207ec2fb6c5fd1d361fa47
Matheus Elyasha <matheuselyasha@gmail.com>
2023-03-26 19:15:55 -0300

    criando repositório de usuário

---
+++ src/modules/accounts/repositories/IUsersRepository.ts
@@ -0,0 +1,12 @@
+import { ICreateUserDTO } from "../dtos/ICreateUserDTO"
+
+
+
+interface IUsersRepository {
+
+    create(data: ICreateUserDTO) : Promise<void>
+
+
+}
+
+export { IUsersRepository }
--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,4 +1,6 @@
 import { container } from 'tsyringe';
+import { UsersRepository } from '../../modules/accounts/repositories/implementations/UsersRepository';
+import { IUsersRepository } from '../../modules/accounts/repositories/IUsersRepository';
 
 import { ICategoriesRepository } from '../../modules/cars/repositories/ICategoriesRepository';
 import { CategoriesRepository } from '../../modules/cars/repositories/implementations/CategoriesRepository';
@@ -16,4 +18,9 @@ container.registerSingleton<ICategoriesRepository>(
 container.registerSingleton<ISpecificationsRepository>(
     "SpecificationsRepository",
     SpecificationsRepository
+)
+
+container.registerSingleton<IUsersRepository>(
+    "UsersRepository",
+    UsersRepository
 )
